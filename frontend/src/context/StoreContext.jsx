2378557d65640ea7ef342406f5b8e25c7ec3a2f9
Rudra Das <154529423+rudradas05@users.noreply.github.com>
2024-11-01 12:08:16 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
-  const url = "http://localhost:4000";
+  const url = "https://epiceats-backend-zz3v.onrender.com";
 
   const [token, setToken] = useState("");
 
