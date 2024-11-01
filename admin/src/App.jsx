625715378619f0b5317854e234b51daddf3e6bed
rudradas05 <dasrudra.stu@gmail.com>
2024-11-01 19:39:35 +0530

    npm run build

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  // const url = "http://localhost:4000";
+  const url = "https://epiceats-backend-0q1i.onrender.com";
 
   return (
     <div>
